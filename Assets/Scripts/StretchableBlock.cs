16527f6a17bc7dec7313da6b34858badbee06660
MoritaTsubasa <94036670+MoritaTsubasa@users.noreply.github.com>
2024-12-06 10:26:54 +0900

    12/06
    最新版12/06

--- Assets/Scripts/StretchableBlock.cs
+++ Assets/Scripts/StretchableBlock.cs
@@ -8,6 +8,7 @@ public class StretchableBlock : MonoBehaviour
     public float recoverySpeed = 2f; // 元に戻る速度
     private Vector3 originalScale; // 元のスケール
     private Rigidbody rb;
+    private bool isStretching = false;// 伸び縮みを有効にするフラグ
 
     // Start is called before the first frame update
     void Start()
@@ -24,10 +25,26 @@ public class StretchableBlock : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // 常に伸び縮みするスケールを適用
-        Vector3 stretchScale = originalScale;
-        float newScaleY = originalScale.y + Mathf.PingPong(Time.time * recoverySpeed,stretchFactor);
-        stretchScale.y = newScaleY;
-        transform.localScale = stretchScale;
+        if(isStretching)
+        {
+            // 常に伸び縮みするスケールを適用
+            Vector3 stretchScale = originalScale;
+            float newScaleY = originalScale.y + Mathf.PingPong(Time.time * recoverySpeed, stretchFactor);
+            stretchScale.y = newScaleY;
+            transform.localScale = stretchScale;
+        }
+    }
+
+    // 伸び縮みを開始
+    public void StartStretching()
+    {
+        isStretching = true;
+    }
+
+    // 伸び縮みを停止
+    public void StopStretching()
+    {
+        isStretching = false;
+        transform.localScale = originalScale;// 元のスケールに戻す
     }
 }
